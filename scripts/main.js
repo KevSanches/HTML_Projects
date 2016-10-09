ef13ce97390ecb18f5d44b14505293d89a97cbca
Kevin Sanches <kevinsanches@gmail.com>
2016-10-09 10:14:15 -0300

    Added a button to get username

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 var image = document.querySelector('img');
+var button = document.querySelector('button');
+var heading = document.querySelector('h1');
+
 
 image.onclick = function() {
 	var src = image.getAttribute('src');
@@ -8,4 +11,18 @@ image.onclick = function() {
 	if (src === 'images/tp.jpg'){
 		image.setAttribute('src', 'images/600_65.jpg');
 	}
+}
+
+function Set_User_Name() {
+	var userName = prompt('Please, type your name');
+	localStorage.setItem('name', userName);
+	heading.textContent = 'The Police is cool, ' + userName;
+}
+
+if (!localStorage.getItem('name')) {
+	Set_User_Name();
+}
+
+button.onclick = function() {
+	Set_User_Name();
 }
