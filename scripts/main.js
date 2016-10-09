f600b6c4fac2c45b72d1bd33db90505c1bcf2c44
Kevin Sanches <kevinsanches@gmail.com>
2016-10-09 09:49:45 -0300

    Adding JS feature, got a bug when the image is click on twice

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'The Police is Freaking Awesome!';
+var image = document.querySelector('img');
+
+image.onclick = function() {
+	var src = image.getAttribute('src');
+	if (src === 'images/600_65.jpg') {
+		image.setAttribute('src', 'images/tp.jpg');
+	}
+	else {
+		image.setAttribute('src', '600_65.jpg');
+
+	}
+}
